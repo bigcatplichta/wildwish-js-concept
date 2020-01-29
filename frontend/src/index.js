78d9b3ccbcc49a92de0278699be933d72cd7c515
bigcatplichta <bigcatplichta@gmail.com>
2020-01-28 19:12:22 -0700

    created wishCard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,24 +25,40 @@ window.onload = () => {
 
 
 function addWishToCard(wish) {
-    // create animal from wish property
+    // create animal and toy objects
     let animal = new Animal(wish.animal)
-    
-    // create toy from wish property
     let toy = new Toy(wish.toy)
-
+    
+    // create card element
+    function wishCard() {
+        let card = document.createElement('div').setAttribute('class', 'card mb-3')
+        this.header = card.appendChild(document.createElement('h3').setAttribute('class', 'card-header'))
+        this.body1 = card.appendChild(document.createElement('div').setAttribute('class', 'card-body'))
+        this.title = body1.appendChild(document.createElement('h5'))
+        this.subtitle = body1.appendChild(document.createElement('h6'))
+        this.body2 = card.appendChild(document.createElement('div').setAttribute('class', 'card-body'))
+        this.para1 = body2.appendChild(document.createElement('p'))
+        this.para2 = body2.appendChild(document.createElement('p').setAttribute('class', 'card-text'))
+        this.progress = body2.appendChild(document.createElement('div').setAttribute('class', 'progress'))
+        this.progress_bar = progress.appendChild(document.createElement('div').setAttribute('class', 'progress-bar'))
+        this.progress_text = progress.appendChild(document.createElement('p').setAttribute('class', 'card-text'))
+        this.button = card.appendChild(document.createElement('button').setAttribute('class', 'btn btn-primary'))
+        this.footer = card.appendChild(document.createElement('div').setAttribute('class', 'card-footer text-muted'))
+        this.link = footer.appendChild(document.createElement('a').setAttribute('class', 'card-link'))
+    }
     // add all properties to card
-    debugger
+    
 }
 
 
 // wishes and animals as objects
+
 class Toy {
     constructor(toy) {
-        toy.id,
-        toy.name,
-        toy.description,
-        toy.cost
+        this.id = toy.id,
+        this.name = toy.name,
+        this.description = toy.description,
+        this.cost = toy.cost
     }
 }
 

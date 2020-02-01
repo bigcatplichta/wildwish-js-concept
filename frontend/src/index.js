4db22bcf0f0bf7dfc002c2c9e0df8163dd1943c0
bigcatplichta <bigcatplichta@gmail.com>
2020-02-01 12:23:48 -0700

    updated 'button' constant to uppercase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 // constants
 const DEFAULT_IMAGE = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E'
-let donateListenerSet
+const DONATE_BUTTON = document.getElementById('create-donation')
 
 function createActiveWishCard(wish) {
     // TODO: abstract all this repeated code or just put this whole function somewhere else
@@ -151,8 +151,7 @@ function openDonateDialog(wish) {
     modal.main.style.display = "block"
 }
 
-// TODO: move this and the rest of variables to a separate file or something
-const button = document.getElementById('create-donation')
+
 class DonationModal {
     
     constructor(wish) {
@@ -169,7 +168,7 @@ class DonationModal {
        
         // create event listener for button
         this.donateListener = this.createDonation.bind(this, wish)
-        button.addEventListener('click', this.donateListener)
+        DONATE_BUTTON.addEventListener('click', this.donateListener)
 
         // syntax: element.addEventListener('listenerType', clickHandler)
 
@@ -259,7 +258,7 @@ class DonationModal {
 
         // remove the event listener that was previously saved
         // refer to 'this.donateListener', which holds the previously created click handler 
-        button.removeEventListener('click', this.donateListener)
+        DONATE_BUTTON.removeEventListener('click', this.donateListener)
     }
 }
 
